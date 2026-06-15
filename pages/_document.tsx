86d596dfcbbec4b7c43fb09fa297e3543a337283
webdevaadil <aadilkhan05@outlook.com>
2026-06-15 20:23:35 +0530

    final logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo.png" />
       </Head>
       <body>
         <Main />
