1c0a9b0df2c5338cbc9096351176a8bad61d2ada
webdevaadil <aadilkhan05@outlook.com>
2026-06-15 22:21:41 +0530

    gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,48 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import "@/globals.css";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(() => new QueryClient(), []);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window !== "undefined" && (window as any).gtag) {
+        (window as any).gtag("config", "G-CF7G9FW9J8", {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-CF7G9FW9J8"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-CF7G9FW9J8', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <Toaster />
         <Sonner />
         <Component {...pageProps} />
@@ -19,3 +53,4 @@ export default function App({ Component, pageProps }: AppProps) {
     </QueryClientProvider>
   );
 }
+
