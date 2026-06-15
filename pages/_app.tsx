956c1fa9f1c4c099d63f8eed467d358cdcd486e9
webdevaadil <aadilkhan05@outlook.com>
2026-06-15 22:25:33 +0530

    gtag 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import "@/globals.css";
 import { useMemo, useEffect } from "react";
 import { useRouter } from "next/router";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = useMemo(() => new QueryClient(), []);
@@ -30,22 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        {/* Google Analytics Script */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-CF7G9FW9J8"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){window.dataLayer.push(arguments);}
-            window.gtag = gtag;
-            gtag('js', new Date());
-            gtag('config', 'G-CF7G9FW9J8', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+
         <Toaster />
         <Sonner />
         <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,21 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/png" href="/logo.png" />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CF7G9FW9J8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', 'G-CF7G9FW9J8', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
